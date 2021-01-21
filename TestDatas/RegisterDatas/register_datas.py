f944c013ff0c104af9cab7863e805d721ad6c4cc
Mo <1664322416@qq.com>
2021-01-22 04:03:17 +0800

    注册正用例设计完成

--- TestDatas/RegisterDatas/register_datas.py
+++ TestDatas/RegisterDatas/register_datas.py
@@ -1,14 +1,14 @@
 # 正常场景测试数据
-normal_data = {'companyName': '测试物流企业有限公司',
-               'LicenseNo': '5453577726662',
-               'linkMan': '张海涛',
-               'contactNumber': '13086275134',
-               'file_path1': r'G:\Item_set\223.jpg',
-               'file_path2': r'G:\Item_set\224.jpg',
-               'file_path3': r'G:\Item_set\225.jpg',
-               'detail': '萧山区人民政府',
+normal_data = {'companyName': '测试四号物流企业有限公司',
+               'LicenseNo': '545111211726662',
+               'linkMan': '张涛涛',
+               'contactNumber': '13082475134',
+               'file_path1': r'D:\Item_set\223.jpg',
+               'file_path2': r'D:\Item_set\224.jpg',
+               'file_path3': r'D:\Item_set\225.jpg',
+               'detail': '日土镇',
                'invoiceTitle': '测试公司',
-               'dutyParagraph': '2323322122133121',
+               'dutyParagraph': '23233555533121',
                'bankOfDeposit': '中国农业银行',
-               'bankAccount': '5464566643334554322'
+               'bankAccount': '546456111114554322'
                }
