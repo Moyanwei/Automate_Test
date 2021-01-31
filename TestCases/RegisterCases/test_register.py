3515a5a97065cb2ef822a5c0d4b57584133cbad4
Mo <1664322416@qq.com>
2021-01-31 22:52:55 +0800

    allure报告编写

--- TestCases/RegisterCases/test_register.py
+++ TestCases/RegisterCases/test_register.py
@@ -1,64 +1,75 @@
 # coding: utf-8
 import pytest
+import allure
 from TestDatas.RegisterDatas import register_datas as REG
 
 
+@allure.feature('注册模块')
+@allure.issue('https://oms.sdhwlw.com/')
 @pytest.mark.usefixtures('register_web')
 class TestRegister:
     """
-     注册功能
-     :param data: 测试数据
-     :param register_web: 注册函数
-     :return: 断言
-     步骤：
+     注册功能测试步骤：
      登录页面-输入用户名- membMobile: ---
      登录页面-输入密码- membPassword: ---
      登录页面-点击登录按钮
      页面跳转至注册信息填写界面-填写注册认证信息
-     断言-首页-获取注册界面（提交）按钮元素，确认元素是否存在
+     断言-首页-获取注册界面提示语、页面元素，确认元素是否存在或提示语是否存在
     """
+    @allure.story('测试注册功能，联系人字段为空')
+    @pytest.mark.parametrize('data', REG.ContactNone_data)
+    def test_register01(self, data, register_web):
+        with allure.step('1.输入公司名称:{}'
+                         '2.输入营业执照号:{}'
+                         '3.')
+            register_web[1].register(data['companyName'], data['LicenseNo'],
+                                 data['linkMan'], data['contactNumber'],
+                                 data['detail'], data['invoiceTitle'],
+                                 data['dutyParagraph'], data['bankOfDeposit'],
+                                 data['bankAccount'], data['file_path1'],
+                                 data['file_path2'], data['file_path3'])
+        assert data['errorMsg'] == register_web[1].Contact_err()   # 断言:联系人不能为空
 
-    def test_register01(self, register_web):
-        register_web[1].register(REG.ContactNone_data['companyName'], REG.ContactNone_data['LicenseNo'],
-                                 REG.ContactNone_data['linkMan'], REG.ContactNone_data['contactNumber'],
-                                 REG.ContactNone_data['detail'], REG.ContactNone_data['invoiceTitle'],
-                                 REG.ContactNone_data['dutyParagraph'], REG.ContactNone_data['bankOfDeposit'],
-                                 REG.ContactNone_data['bankAccount'], REG.ContactNone_data['file_path1'],
-                                 REG.ContactNone_data['file_path2'], REG.ContactNone_data['file_path3'])
-        assert REG.ContactNone_data['errorMsg'] == register_web[1].Contact_err()
+    @allure.story('测试注册功能，联系电话字段为空')
+    @pytest.mark.parametrize('data', REG.ContactPhoneNone_data)
+    def test_register02(self, data, register_web):
+        register_web[1].register(data['companyName'], data['LicenseNo'],
+                                 data['linkMan'], data['contactNumber'],
+                                 data['detail'], data['invoiceTitle'],
+                                 data['dutyParagraph'], data['bankOfDeposit'],
+                                 data['bankAccount'], data['file_path1'],
+                                 data['file_path2'], data['file_path3'])
+        assert data['errorMsg'] == register_web[1].ContactPhone_err()    # 断言:联系方式不能为空
 
-    def test_register02(self, register_web):
-        register_web[1].register(REG.ContactPhoneNone_data['companyName'], REG.ContactPhoneNone_data['LicenseNo'],
-                                 REG.ContactPhoneNone_data['linkMan'], REG.ContactPhoneNone_data['contactNumber'],
-                                 REG.ContactPhoneNone_data['detail'], REG.ContactPhoneNone_data['invoiceTitle'],
-                                 REG.ContactPhoneNone_data['dutyParagraph'], REG.ContactPhoneNone_data['bankOfDeposit'],
-                                 REG.ContactPhoneNone_data['bankAccount'], REG.ContactPhoneNone_data['file_path1'],
-                                 REG.ContactPhoneNone_data['file_path2'], REG.ContactPhoneNone_data['file_path3'])
-        assert REG.ContactPhoneNone_data['errorMsg'] == register_web[1].ContactPhone_err()
+    @allure.story('测试注册功能，运营中心字段为空')
+    @pytest.mark.parametrize('data', REG.YyzxNone_data)
+    def test_register03(self, data, register_web):
+        register_web[1].registerxYyzx_None(data['companyName'], data['LicenseNo'],
+                                           data['linkMan'], data['contactNumber'],
+                                           data['detail'], data['invoiceTitle'],
+                                           data['dutyParagraph'], data['bankOfDeposit'],
+                                           data['bankAccount'], data['file_path1'],
+                                           data['file_path2'], data['file_path3'])
+        assert data['errorMsg'] == register_web[1].Yyzx_err()   # 断言:运营中心不能为空
 
-    def test_register03(self, register_web):
-        register_web[1].registerxYyzx_None(REG.YyzxNone_data['companyName'], REG.YyzxNone_data['LicenseNo'],
-                                           REG.YyzxNone_data['linkMan'], REG.YyzxNone_data['contactNumber'],
-                                           REG.YyzxNone_data['detail'], REG.YyzxNone_data['invoiceTitle'],
-                                           REG.YyzxNone_data['dutyParagraph'], REG.YyzxNone_data['bankOfDeposit'],
-                                           REG.YyzxNone_data['bankAccount'], REG.YyzxNone_data['file_path1'],
-                                           REG.YyzxNone_data['file_path2'], REG.YyzxNone_data['file_path3'])
-        assert REG.YyzxNone_data['errorMsg'] == register_web[1].Yyzx_err()
+    @allure.story('测试注册功能，公司名称字段已存在且重复时')
+    @pytest.mark.parametrize('data', REG.YyzxNone_data)
+    def test_register04(self, data, register_web):
+        register_web[1].register(data['companyName'], data['LicenseNo'],
+                                 data['linkMan'], data['contactNumber'],
+                                 data['detail'], data['invoiceTitle'],
+                                 data['dutyParagraph'], data['bankOfDeposit'],
+                                 data['bankAccount'], data['file_path1'],
+                                 data['file_path2'], data['file_path3'])
+        assert data['errorMsg'] == register_web[1].CompanyName_err()   # 断言:公司名称已存在
 
-    def test_register04(self, register_web):
-        register_web[1].register(REG.CompanyName_data['companyName'], REG.CompanyName_data['LicenseNo'],
-                                 REG.CompanyName_data['linkMan'], REG.CompanyName_data['contactNumber'],
-                                 REG.CompanyName_data['detail'], REG.CompanyName_data['invoiceTitle'],
-                                 REG.CompanyName_data['dutyParagraph'], REG.CompanyName_data['bankOfDeposit'],
-                                 REG.CompanyName_data['bankAccount'], REG.CompanyName_data['file_path1'],
-                                 REG.CompanyName_data['file_path2'], REG.CompanyName_data['file_path3'])
-        assert REG.CompanyName_data['errorMsg'] == register_web[1].CompanyName_err()
-
-    def test_register05(self, register_web):
-        register_web[1].register(REG.Normal_data['companyName'], REG.Normal_data['LicenseNo'],
-                                 REG.Normal_data['linkMan'], REG.Normal_data['contactNumber'],
-                                 REG.Normal_data['detail'], REG.Normal_data['invoiceTitle'],
-                                 REG.Normal_data['dutyParagraph'], REG.Normal_data['bankOfDeposit'],
-                                 REG.Normal_data['bankAccount'], REG.Normal_data['file_path1'],
-                                 REG.Normal_data['file_path2'], REG.Normal_data['file_path3'])
-        assert register_web[1].submit_is_exist()
+    @allure.story('测试注册功能，注册成功')
+    @pytest.mark.parametrize('data', REG.Normal_data)
+    def test_register05(self, data, register_web):
+        register_web[1].register(data['companyName'], data['LicenseNo'],
+                                 data['linkMan'], data['contactNumber'],
+                                 data['detail'], data['invoiceTitle'],
+                                 data['dutyParagraph'], data['bankOfDeposit'],
+                                 data['bankAccount'], data['file_path1'],
+                                 data['file_path2'], data['file_path3'])
+        assert register_web[1].submit_is_exist()   # 断言:注册成功后某一元素存在
